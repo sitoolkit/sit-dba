e9e191e6e73ac4abda6f021d8dd091e5c502fe4b
Yuichi Kuwahara <yuichi.kuwahara.0@gmail.com>
2020-12-27 15:16:35 +0900

    Externalize csv-loader

--- sit-dba-csvloader/src/main/java/io/sitoolkit/dba/LogCallback.java
+++
@@ -1,6 +0,0 @@
-package io.sitoolkit.dba;
-
-public interface LogCallback {
-  
-  void info(String log);
-}
--- sit-dba-migration/src/main/java/db/migration/V2__AddRecords.java
+++ sit-dba-migration/src/main/java/db/migration/V2__AddRecords.java
@@ -5,7 +5,7 @@ import org.flywaydb.core.api.logging.LogFactory;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
 
-import io.sitoolkit.dba.CsvLoader;
+import io.sitoolkit.csv.core.CsvLoader;
 
 @SuppressWarnings("squid:S101")
 public class V2__AddRecords extends BaseJavaMigration {
